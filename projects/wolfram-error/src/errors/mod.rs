731cb071821c19f25d2d1c2bb814172e1ccbac84
Aster <aster@vers.site>
2020-08-18 18:42:27 +0000

    🐤 Set file and span for wolfram error kind

--- projects/wolfram-error/src/errors/mod.rs
+++ projects/wolfram-error/src/errors/mod.rs
@@ -1,6 +1,84 @@
-pub struct WolframError {}
+use diagnostic::{FileID, FileSpan};
+use std::ops::Range;
+
+pub struct WolframError {
+    kind: Box<WolframErrorKind>,
+}
+
+impl WolframError {
+    /// Get the kind of the error
+    pub fn kind(&self) -> &WolframErrorKind {
+        &*self.kind
+    }
+}
 
 pub enum WolframErrorKind {
-    RuntimeError {},
+    RuntimeError { message: String },
     SyntaxError { message: String, span: FileSpan },
+}
+
+impl WolframError {
+    pub fn runtime_error<S>(message: String) -> Self
+    where
+        S: ToString,
+    {
+        Self { kind: Box::new(WolframErrorKind::RuntimeError { message }) }
+    }
+    pub fn syntax_error<S>(message: String) -> Self
+    where
+        S: ToString,
+    {
+        Self { kind: Box::new(WolframErrorKind::SyntaxError { message, span }) }
+    }
+    pub fn set_file(&mut self, file: FileID) {
+        match &mut self.kind {
+            WolframErrorKind::RuntimeError { .. } => {}
+            WolframErrorKind::SyntaxError { span, .. } => span.set_file(file),
+        }
+    }
+    pub fn with_file(mut self, file: FileID) -> Self {
+        self.set_file(file);
+        self
+    }
+    pub fn set_span(&mut self, range: Range<usize>) {
+        match &mut self.kind {
+            WolframErrorKind::RuntimeError { .. } => {}
+            WolframErrorKind::SyntaxError { span, .. } => span.set_range(range),
+        }
+    }
+    pub fn with_span(mut self, range: Range<usize>) -> Self {
+        self.set_span(range);
+        self
+    }
+    pub fn set_file_span(&mut self, range: FileSpan) {
+        match &mut self.kind {
+            WolframErrorKind::RuntimeError { .. } => {}
+            WolframErrorKind::SyntaxError { span, .. } => span.set_range(range),
+        }
+    }
+    pub fn with_file_span(mut self, range: FileSpan) -> Self {
+        self.set_span(range);
+        self
+    }
+}
+
+impl WolframErrorKind {
+    pub fn set_file(&mut self, file: FileID) {
+        match self {
+            Self::RuntimeError { .. } => {}
+            Self::SyntaxError { span, .. } => span.set_file(file),
+        }
+    }
+    pub fn set_span(&mut self, range: Range<usize>) {
+        match self {
+            Self::RuntimeError { .. } => {}
+            Self::SyntaxError { span, .. } => span.set_range(range),
+        }
+    }
+    pub fn set_file_span(&mut self, range: FileSpan) {
+        match self {
+            Self::RuntimeError { .. } => {}
+            Self::SyntaxError { span, .. } => span.set_range(range),
+        }
+    }
 }
--- projects/wolfram-parser/src/parser/from_str/mod.rs
+++ projects/wolfram-parser/src/parser/from_str/mod.rs
@@ -1,30 +1,25 @@
-use std::str::FromStr;
+use crate::{
+    ast::WolframStatements,
+    parser::codegen::{RootNode, StatementsNode, WolframRule},
+};
 use diagnostic::{FileCache, FileID};
+use std::str::FromStr;
 use yggdrasil_rt::YggdrasilError;
-use crate::ast::WolframStatements;
-use crate::parser::codegen::{RootNode, StatementsNode, WolframRule};
 
 impl FromStr for WolframStatements {
     type Err = YggdrasilError<WolframRule>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         let root = RootNode::from_str(s)?;
         let mut out = Vec::with_capacity(root.statements.len());
-        for x in root.statements {
-
-
-        }
+        for x in root.statements {}
     }
 }
 
 impl WolframStatements {
-    pub fn from_cache(file: FileID, cache: &FileCache) -> Result<WolframStatements, YggdrasilError<WolframRule>> {
-        let text = cache.fetch(&file)
+    pub fn from_cache(file: FileID, cache: &FileCache) -> Wolfram<WolframStatements, YggdrasilError<WolframRule>> {
+        let text = cache.fetch(&file)?;
     }
 }
 
-
-impl StatementsNode {
-
-}
+impl StatementsNode {}
